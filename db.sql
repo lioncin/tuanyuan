d276a34ead6d4bded4724dea8c40e543e780ec2f
ccp-linx <601211505@qq.com>
2019-03-31 20:37:41 +0800

    fix bug

---
+++ db.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE tuanyuanzheng DEFAULT CHARACTER SET utf8;
+GRANT ALL PRIVILEGES ON tuanyuanzheng.* TO 'tuanyuanzheng'@'%' IDENTIFIED BY 'Password$1';
+FLUSH PRIVILEGES;
+USE tuanyuanzheng;

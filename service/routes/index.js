bab723fb28aae1f6dcc429e75689815f55ea7391
ccp-linx <601211505@qq.com>
2019-04-06 00:27:49 +0800

    fix bug

--- service/routes/index.js
+++ service/routes/index.js
@@ -1,59 +1,83 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var router = express.Router();
+var mysql = require('mysql');
+var mysqlParams = {
+  host     : 'localhost',
+  user     : 'root',
+  password : '123456',
+  database : 'tuanyuanzheng'
+};
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+router.post('/user',urlencodedParser, function(req, res, next){
   var addSql = 'INSERT INTO CM_USER(WECHAT_ID,NAME,GENDER,HOME,COMPANY,BIRTHDAY,RUTUAN,PHOTO_URL,CREATED_AT,CREATED_BY) VALUES(?,?,?,?,?,?,?,?,NOW(),1)';
   var addSqlParams = [];
-  for(var key in req.query){
-    if(key=='wechat'){
-      addSqlParams.push(req.query[key]);
+  for(var key in req.body){
+    if(key=='id'){
+      addSqlParams.push(req.body[key]);
     }
     if(key=='name'){
-      addSqlParams.push(req.query[key]);
+      addSqlParams.push(req.body[key]);
     }
     if(key=='gender'){
-      addSqlParams.push(req.query[key]);
+      addSqlParams.push(req.body[key]);
     }
     if(key=='home'){
-      addSqlParams.push(req.query[key]);
+      addSqlParams.push(req.body[key]);
     }
     if(key=='company'){
-      addSqlParams.push(req.query[key]);
+      addSqlParams.push(req.body[key]);
     }
     if(key=='birthday'){
-      addSqlParams.push(req.query[key]);
+      addSqlParams.push(req.body[key]);
     }
     if(key=='rutuan'){
-      addSqlParams.push(req.query[key]);
+      addSqlParams.push(req.body[key]);
     }
     if(key=='photo'){
-      addSqlParams.push(req.query[key]);
+      addSqlParams.push(req.body[key]);
     }
   }
-  
-  var mysql = require('mysql');
-  var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : '123456',
-    database : 'tuanyuanzheng'
-  });
-  connection.connect();
 
+  var connection = mysql.createConnection(mysqlParams);
+  connection.connect();
   console.log(addSqlParams);
   connection.query(addSql,addSqlParams,function (err, result) {
     if(err){
       console.log('[INSERT ERROR] - ',err.message);
       return;
-    }        
- 
-  console.log('--------------------------INSERT----------------------------');
-  console.log('INSERT ID:',result);        
-  console.log('-----------------------------------------------------------------\n\n');  
-});
+    }
+  });
   connection.end();
   res.render('index', { title: 'Express' });
+});
+
+//get user info
+router.get('/user/:id', function(req, res, next){
+  const id = req.params.id;
+  if(id){
+    const sql = 'SELECT * FROM cm_user WHERE wechat_id='+id;
+    var connection = mysql.createConnection(mysqlParams);
+    connection.query(sql,function (err, result) {
+      res.send(result);
+    });
+    connection.end();
+  }else{
+    res.send('error');
+  }
+});
+
+//save user info
+// router.post('/user',urlencodedParser, function(req, res, next){
+//   const body = req.body;
+//   res.send(req.body);
+// });
+
+//upload user photo
+router.post('/user/photo', function(req, res, next){
+
 });
 
 module.exports = router;
